114a853929c50629ac1432cc6505f163d713d070
Anthony <anthonyhch12@gmail.com>
2022-12-21 13:33:17 -0600

    The change in parametres was done

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,48 @@
 public class Main {
     public static void main(String[] args) {
 
+        Client client = new Client();
+
+        Worker worker = new Worker();
+
+        System.out.println("Client class");
+        client.Show("Tony", 20, 8993272,292);
+
     }
+
 }
 
 abstract class Person{
     int age;
     String name;
     int phoneNumber;
+
+
 }
 
-class client extends Person{
+class Client extends Person{
     int credit;
 
-    public void Show(){
+
+    public void Show(String name, int age, int phoneNumber, int credit){
         System.out.println ("Your name: " + name);
         System.out.println ("Your age: " + age);
-        System.out.println ("Your phone number: " + name);
-        System.out.println ("Your credit: " + name);
+        System.out.println ("Your phone number: " + phoneNumber);
+        System.out.println ("Your credit: " + credit);
     }
 }
+
+final class Worker extends Person{
+
+    int salary;
+
+    public void Show(String name, int age, int phoneNumber, int salary){
+        System.out.println ("Your name: " + name);
+        System.out.println ("Your age: " + age);
+        System.out.println ("Your phone number: " + phoneNumber);
+        System.out.println ("Your salary: " + salary);
+    }
+
+}
+
 
